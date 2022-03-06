0f92da8e4431c8fe84b5e16bc25781f8390bd56c
Johann Rekowski <johann.rekowski@gmail.com>
2022-03-06 07:52:51 +0100

    Minor variable name change

--- src/cli.rs
+++ src/cli.rs
@@ -15,7 +15,7 @@ pub fn get_matches() -> ArgMatches {
                     value
                         .parse::<u16>()
                         .map(|port| PORT_RANGE.contains(&port))
-                        .map_err(|e| e.to_string())
+                        .map_err(|err| err.to_string())
                         .and_then(|result| match result {
                             true => Ok(()),
                             false => Err(format!(
